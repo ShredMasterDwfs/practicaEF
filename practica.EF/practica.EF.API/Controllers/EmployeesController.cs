9062beca11049ff00bed39843a9b70ca1df59288
ShredMasterDwfs <45758066+ShredMasterDwfs@users.noreply.github.com>
2022-10-16 13:56:28 -0300

    api implementada

---
+++ practica.EF/practica.EF.API/Controllers/EmployeesController.cs
@@ -0,0 +1,94 @@
+﻿using practica.EF.Entities;
+using practica.EF.Logic;
+using practica.EF.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace practica.EF.API.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+
+        EmployeesLogic logic = new EmployeesLogic();
+        // GET: /api/Employees
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            try
+            {
+                List<Employees> listEmployees = logic.GetAll();
+                List<EmployeesResponse> response = listEmployees.Select(e => new EmployeesResponse
+                {
+                    EmployeeID = e.EmployeeID,
+                    LastName = e.LastName,
+                    FirstName = e.FirstName                  
+                }).ToList();
+
+                return Ok(response);
+            }
+            catch (Exception)
+            {
+                return Content(HttpStatusCode.BadRequest, "Error");
+            }
+        }
+
+        // POST: /api/Employees
+        [HttpPost]
+        public IHttpActionResult Add(EmployeesRequest newEmployees)
+        {
+            try
+            {
+                Employees employee = new Employees
+                {
+                    LastName = newEmployees.LastName,
+                    FirstName = newEmployees.FirstName                    
+                };
+                logic.Add(employee);
+                return Content(HttpStatusCode.Created, employee);
+            }
+            catch (Exception)
+            {
+                return Content(HttpStatusCode.BadRequest, "Error");
+            }
+        }
+
+        // POST: /api/Employees/{id}
+        [HttpPost]
+        public IHttpActionResult Delete(string id)
+        {
+            try
+            {
+                logic.Delete(id);
+                return Content(HttpStatusCode.OK, "Employee deleted successfully!");
+            }
+            catch (Exception)
+            {
+                return Content(HttpStatusCode.BadRequest, "Error");
+            }
+        }
+
+        // PUT: /api/Employees/{id}
+        [HttpPut]
+        public IHttpActionResult Update(EmployeesRequest newEmployees)
+        {
+            try
+            {
+                Employees employee = new Employees
+                {
+                    EmployeeID = newEmployees.EmployeeID,
+                    LastName = newEmployees.LastName,
+                    FirstName = newEmployees.FirstName                    
+                };
+                logic.Update(employee);
+                return Content(HttpStatusCode.OK, employee);
+            }
+            catch (Exception)
+            {
+                return Content(HttpStatusCode.BadRequest, "Error");
+            }
+        }
+    }
+}
---
+++ practica.EF/practica.EF.API/Models/EmployeesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace practica.EF.API.Models
+{
+    public class EmployeesResponse
+    {
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
